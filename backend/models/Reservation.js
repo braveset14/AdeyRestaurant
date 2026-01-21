af3e97ec0b73e7fced5d38e21bf20b4989941d73
braveset14 <braveset14@gmail.com>
2026-01-21 11:08:00 +0300

    Added Mongodb database for smooth reservation page functionality.

---
+++ backend/models/Reservation.js
@@ -0,0 +1,10 @@
+const mongoose=require('mongoose');
+
+const reservationSchema=new mongoose.Schema({
+  name: { type: String, required: true },
+  guests: { type: Number, required: true },
+  date: { type: String, required: true },
+  time: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now }
+});
+module.exports=mongoose.model('Reservation',reservationSchema);
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,29 @@
 const express=require('express');
+const mongoose = require('mongoose');
 const cors=require('cors');
 require('dotenv').config();
 
-const app=express();
 const PORT=process.env.PORT || 5000;
+const Reservation=require('./models/Reservation');
 
+const app=express();
 app.use(cors());
 app.use(express.json());
 
+mongoose.connect(process.env.MONGO_URI)
+      .then(()=>console.log("Connected to Adey's Database!"))
+      .catch(err=> console.error("Database connection error:", err));
+
+app.post('/api/reservations',async(req,res)=>{
+    try{
+        const { name, guests, date, time } = req.body;
+    const newReservation = new Reservation({ name, guests, date, time });
+    await newReservation.save();
+    res.status(201).json({ message: "Reservation saved successfully!" });
+    } catch(error){
+        res.status(500).json({ error: "Failed to save reservation." });
+    }
+})
 app.get('/',(req,res)=>{
     res.send("Adey restaurant backend is running ...");
 });
