baeb63c56723e962e854f41c4a2ce2e74f371cd1
braveset14 <braveset14@gmail.com>
2026-01-27 17:05:16 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -118,12 +118,17 @@ app.delete('/api/admin/reservations/:id', async (req, res) => {
     }
 });
 
-const transporter=nodemailer.createTransport({
-    service:'gmail',
-    auth:{
-        user:process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    }
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // MUST be false for 587
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  },
+  tls: {
+    rejectUnauthorized: false
+  }
 });
 
 app.post('/api/reservations/forgot-info', async (req, res) => {
